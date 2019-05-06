505b4898990d4714c656545b5d797408869c32f1
billy <billydev@gmail.com>
2019-05-06 10:58:44 -0600

    Updates

---
+++ precache-manifest.8aa5c2c53f5ee3debd367df457d95017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "51e46eb9b4ebeada208a",
+    "url": "/react-tutorial/static/js/main.62d83257.chunk.js"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "51e46eb9b4ebeada208a",
+    "url": "/react-tutorial/static/css/main.a6a3adad.chunk.css"
+  },
+  {
+    "revision": "2e549dbf61bfd894755c001d044f3492",
+    "url": "/react-tutorial/index.html"
+  }
+];
