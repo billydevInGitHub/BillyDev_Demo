de44d9bbeba0e268de3634fc94161a62d7ee3911
Billy Li <bli@enabil.com>
2019-06-23 19:02:44 -0600

    create Dockerfile to deploy to the cloud

--- src/_helpers/Constants.js
+++ src/_helpers/Constants.js
@@ -1,6 +1,7 @@
-export const SERVER_URL='http://localhost:8180'
+//export const SERVER_URL='http://localhost:8180'
 export const RELATIVE_PATH='/BillyDev_Demo'
 //export const SERVER_URL='http://ec2-52-39-223-242.us-west-2.compute.amazonaws.com:8087'
 //export const SERVER_URL='https://localhost:8443'
 //export const SERVER_URL='https://ec2-54-190-194-139.us-west-2.compute.amazonaws.com:8443'
+export const SERVER_URL='http://spring-boot-50216exp008:8180'
 export const FAKEBACKEND_JWT_EXPIRY_MILLISECONDS=6000; 
